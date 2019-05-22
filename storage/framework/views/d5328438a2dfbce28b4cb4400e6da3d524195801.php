1e47d36249721aacccbea927d66a6165fdde6296
RuanCarlosPorto <ruancarlosporto@hotmail.com>
2019-05-22 19:48:48 -0300

    template

---
+++ storage/framework/views/d5328438a2dfbce28b4cb4400e6da3d524195801.php
@@ -0,0 +1,64 @@
+<div class="modal fade mAmber" id="inserirVeiculo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title w-100">Inserir Veiculo</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-8">
+                        <div class="md-form">
+                            <i class="fas fa-clipboard-list prefix"></i>
+                            <input type="text" id="marcaVeiculo" class="form-control">
+                            <label for="marcaVeiculo">Marca Veículo</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-8">
+                        <div class="md-form">
+                            <i class="fas fa-taxi prefix"></i>
+                            <input type="text" id="modeloVeiculo" class="form-control">
+                            <label for="modeloVeiculo">Modelo Veículo</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-8">
+                        <div class="md-form">
+                            <i class="fas fa-tools prefix"></i>
+                            <input type="text" id="placaVeiculo" class="form-control">
+                            <label for="placaVeiculo">Placa Veículo</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-8">
+                        <div class="md-form">
+                            <i class="fas fa-palette prefix"></i>
+                            <input type="text" id="corVeiculo" class="form-control">
+                            <label for="corVeiculo">Cor Veículo</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-8">
+                        <button type="button" class="btn btn-success btn-lg btn-block" id="btnSalvarVeiculo">Salvar Veículo</button>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-amber btn-sm fechar" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div><?php /**PATH /var/www/html/TaxiOut/resources/views/taxista/modais.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e62305217382416f5cd64eba3242a62cf1364ac3.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-4"></div>
+    <div class="col top-more-4">
+        <h2>Agendamento de viagem</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-4">
+
+    </div>
+</div>
+<?php /**PATH /var/www/html/TaxiOut/resources/views/taxista/nova_viagem.blade.php ENDPATH**/ ?>
