072c51a3f019b7d63f11317257830e5336b15f04
RuanCarlosPorto <ruancarlosporto@hotmail.com>
2019-05-22 18:54:03 -0300

    commiting

--- storage/framework/views/6c2d3ac51066c83f458ce394823801985aed9018.php
+++ storage/framework/views/6c2d3ac51066c83f458ce394823801985aed9018.php
@@ -7,128 +7,133 @@
     </div>
 
     <div class="col-5 inputs">
-    <form class="hrDiv">
         <div class="row">
-            <div class="col-4">
-                <div class="md-form">
-                    <input type="text" class="form-control" id="iptLogin">
-                    <label for="iptLogin">Login</label>
+            <form class="hrDiv">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="md-form">
+                            <input type="text" class="form-control" id="iptLogin">
+                            <label for="iptLogin">Login</label>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="md-form">
+                            <input type="password" class="form-control" id="iptSenha">
+                            <label for="iptSenha">Senha</label>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="md-form">
+                            <button id="btnLogin" type="button" class="btn btn-yellow btn-sm">Entrar</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-4">
-                <div class="md-form">
-                    <input type="password" class="form-control" id="iptSenha">
-                    <label for="iptSenha">Senha</label>
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="md-form">
-                    <button id="btnLogin" type="button" class="btn btn-yellow btn-sm">Entrar</button>
-                </div>
-            </div>
-        </div>
-    </form>  
-    <hr>
-
-    <form id="ContainerFormInicial">
-        <div class="row margin2">
-            <div class="col-2"></div>
-            <div class="col">
-                <img src="/img/taxiPng.png" id="miniTaxiInicial">
-            </div>
-        </div>
-        <div class="row margin1">
-            <div class="col-2"></div>
-            <div class="col-8">
-                <h2 class="font-weight-bold">Taxista ou Cliente, venha viajar pelo mundo agora.</h2>
-                <br>
-                <h5 class="font-weight-bold">Faça parte do TaxiOut</h5>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-2"></div>
-            <div class="col-8">
-                <div class="md-form">
-                    <button type="button" class="btn btn-success btn-lg btn-block" id="btnInscrever">Inscrever-se</button>
-                </div>
-            </div>
+            </form>
         </div>
 
-    </form>
+        <hr>
+        <form id="ContainerFormInicial">
+            <div class="row margin2">
+                <div class="col-2"></div>
+                <div class="col">
+                    <img src="/img/taxiPng.png" id="miniTaxiInicial">
+                </div>
+            </div>
+            <div class="row margin1">
+                <div class="col-2"></div>
+                <div class="col-8">
+                    <h2 class="font-weight-bold">Taxista ou Cliente, venha viajar pelo mundo agora.</h2>
+                    <br>
+                    <h5 class="font-weight-bold">Faça parte do TaxiOut</h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+                    <div class="md-form">
+                        <button type="button" class="btn btn-success btn-lg btn-block" id="btnInscrever">Inscrever-se
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
 
-    <form class="to-Hide" id="containerInscrever">
-        <div class="row text-center">
-            <div class="col">
-                <h1>Increver-se</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6"><hr></div>
-        </div>
-        <div class="row">
-            <div class="col-4"></div>
-            <div class="custom-control custom-radio custom-control-inline">
-                <input checked type="radio" class="custom-control-input" id="rbnTaxista" name="inlineDefaultRadiosExample">
-                <label class="custom-control-label" for="rbnTaxista">Taxista</label>
-            </div>
-            <div class="custom-control custom-radio custom-control-inline">
-                <input type="radio" class="custom-control-input" id="rbnCliente" name="inlineDefaultRadiosExample">
-                <label class="custom-control-label" for="rbnCliente">Cliente</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6">
-                <div class="md-form">
-                    <i class="fas fa-user-plus prefix grey-text"></i>
-                    <input type="text" id="nomeInscrever" class="form-control">
-                    <label for="nomeInscrever">Nome</label>
+        <form class="to-Hide" id="containerInscrever">
+            <div class="row text-center">
+                <div class="col">
+                    <h1>Increver-se</h1>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6">
-                <div class="md-form">
-                    <i class="fas fa-unlock prefix grey-text"></i>
-                    <input type="password" id="senhaInscrever" class="form-control">
-                    <label for="senhaInscrever">Senha</label>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <hr>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6">
-                <div class="md-form">
-                    <i class="fas fa-unlock prefix grey-text"></i>
-                    <input type="password" id="confirmSenha" class="form-control">
-                    <label for="confirmSenha">Confirmar Senha</label>
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="custom-control custom-radio custom-control-inline">
+                    <input checked type="radio" class="custom-control-input" id="rbnTaxista"
+                           name="inlineDefaultRadiosExample">
+                    <label class="custom-control-label" for="rbnTaxista">Taxista</label>
+                </div>
+                <div class="custom-control custom-radio custom-control-inline">
+                    <input type="radio" class="custom-control-input" id="rbnCliente" name="inlineDefaultRadiosExample">
+                    <label class="custom-control-label" for="rbnCliente">Cliente</label>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6">
-                <div class="md-form">
-                    <i class="fas fa-at prefix grey-text"></i>
-                    <input type="text" id="emailInscrever" class="form-control">
-                    <label for="emailInscrever">Email</label>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <div class="md-form">
+                        <i class="fas fa-user-plus prefix grey-text"></i>
+                        <input type="text" id="nomeInscrever" class="form-control">
+                        <label for="nomeInscrever">Nome</label>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-3"></div>
-            <div class="col-4">
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <div class="md-form">
+                        <i class="fas fa-unlock prefix grey-text"></i>
+                        <input type="password" id="senhaInscrever" class="form-control">
+                        <label for="senhaInscrever">Senha</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <div class="md-form">
+                        <i class="fas fa-unlock prefix grey-text"></i>
+                        <input type="password" id="confirmSenha" class="form-control">
+                        <label for="confirmSenha">Confirmar Senha</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <div class="md-form">
+                        <i class="fas fa-at prefix grey-text"></i>
+                        <input type="text" id="emailInscrever" class="form-control">
+                        <label for="emailInscrever">Email</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-4">
                     <button type="button" id="btnCadastrar" class="btn btn-yellow btn-sm">Inscrever-se</button>
+                </div>
+                <div class="col-3">
+                    <button type="button" class="btn btn-deep-orange btn-sm" id="btnVoltar">Voltar</button>
+                </div>
             </div>
-            <div class="col-3">
-                <button type="button" class="btn btn-deep-orange btn-sm" id="btnVoltar">Voltar</button>
-            </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
 
-<?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<script type="text/javascript" src="/js/login.js"></script>
+    <?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <script type="text/javascript" src="/js/login.js"></script>
 <?php /**PATH /var/www/html/TaxiOut/resources/views/user/login.blade.php ENDPATH**/ ?>
